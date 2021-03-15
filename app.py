7af52633f9d80bc1b7987ae6cc200c88ad424956
CodeJohnathan <33619145+CodeJohnathan@users.noreply.github.com>
2021-03-15 21:17:39 +0200

    Working with files

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 #we will be covering mostly important syntax gaps.
 
-#lists ranges in python
-Neighbors = ["Isaac","Johnny","Lara","Debby","Avihay"]
-print(Neighbors[1:]) #will print anything from that index forward.
-print(Neighbors[2:4]) #will print values stored in these indexes, excluding the last.
-Neighbors.append("Jacob") #appends another item at the end
-Neighbors.insert(3,"Me") #inserts the new item at a given index. e.g. (3, "NewItem")
-# .pop(),.remove(), .count() , .sort(), .reversed()
-Neighbors.extend([]) #allows to add another list (iterable) to our existing list
-newNeighbors = Neighbors.copy()
-
+'''
 #In general, [] paranthesis are for lists, () paranthesis are for tuples, and {} paranthesis are for dictionaries.
+#try/catch -> try/except
+'''
+#Python gives us the ability to check what permissions of a file we have.
+#Incase we want to read from a file, we can simply ask, is he readable? is he writable?
+#If the file does not exist, indicate it by mode w+, which will create the new file.
+new_file = open("Members.txt","r")
+print(new_file.readable())
+print(new_file.writable())
+new_file.close()
